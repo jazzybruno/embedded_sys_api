808048d3ec6622a02dca34bcc975d73508061174
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-10-12 20:53:42 +0200

    [Add] Added the database source control

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,22 @@
 import express , {Request , Response , Application} from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+import auth from './middlewares/middlewares';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+require('dotenv').config();
+var sqlite3 = require('sqlite3').verbose();
+
+const DBSOURCE = "usersdb.sql";
+
+app.use(express.urlencoded(),
+cors({
+    origin: 'localhost:3000'
+}));
 
 app.get('/get' , (req : Request , res : Response) => {
     res.send('Hello World');
