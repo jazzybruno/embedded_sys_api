adba36da217555f6987f4a815254df9f04a622bc
jazzybruno <95282931+jazzybruno@users.noreply.github.com>
2023-10-18 16:15:45 +0200

    [Add] Implemented the user login and signup

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import 'reflect-metadata'
 import bodyParser from 'body-parser';
 import useRouter from './routes/user.route';
 import statsRouter from './routes/stats.route';
+import path from 'path';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -22,6 +23,27 @@ app.use(express.urlencoded(),
 cors({
     origin: 'localhost:3000'
 }))
+
+// The views Rendering Engine
+const publicDir = path.join(__dirname, '../public');
+
+app.use(express.static(publicDir));
+
+// the views routes 
+app.get('/dashboard' ,  (req , res) => {
+    res.render('index')
+})
+
+app.get('/login' , (req , res)=>{
+    res.render('login')
+})
+
+app.get('/register' , (req , res)=>{
+    res.render('register')
+})
+
+
+app.set('view engine', 'hbs');
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
